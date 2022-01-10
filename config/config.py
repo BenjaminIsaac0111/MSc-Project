ca0809656cbb7832f72f087ae58c478a666da68c
benjaminwilson <benjamintaya0111@gmail.com>
2022-01-10 16:41:51 +0000

    -Edits to the config loaders defaulting procedure.

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 import yaml
 
 
@@ -11,18 +12,18 @@ def load_config(file=None):
             except yaml.YAMLError as exc:
                 print(exc)
     except FileNotFoundError:
-        return {'CACHE': 'cache\\',
-                'DATA_DIR': 'data\\',
+        print('Defaulting...')
+        return {'DATA_DIR': 'data\\',
                 'TRAINING_DATA': 'models\\TrainingData.txt',
                 'TEST_DATA': 'models\\TestData.txt',
-                'PATCHES_DIR': 'data\\HGDL3\\',
-                'XML_DIRECTORY': 'data\\MIM_XML_CONVERSIONS\\',
+                'PATCHES_DIR': 'data\\',
                 'ASSOCIATED_FILE_PATTERN': 'w.*scores.xml',
                 'DOWNSAMPLE_FACTOR': 2,
                 'PATCH_SIZE':
                     {'WIDTH': 512,
                      'HEIGHT': 1024},
-                'MASK': {'RADIUS': 32}}
+                'EXPAND_OUT_FACTOR': 2
+                }
 
 
 CONFIG = load_config(file='config\\default_configuration.yaml')
