8032d5ec19c65e8f6ef570e82b62ee29adcfa817
benjaminwilson <benjamintaya0111@gmail.com>
2022-02-02 12:23:21 +0000

    -Added setup.py.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='SVSLoader',
+    version='0.0.1',
+    packages=['tests', 'config', 'scripts', 'SVSLoader', 'SVSLoader.Utils', 'SVSLoader.Loaders', 'SVSLoader.Processing',
+              'SVSLoader.InferenceUtils'],
+    url='',
+    license='',
+    author='Benjamin Isaac Wilson',
+    author_email='benjamintaya0111@gmail.com',
+    description='SVS Whole Slide Image Loader and Processor - Various utilities to load large collections of SVS images'
+                'and annotations files with an emphasis on automated file search and listing. Currently being developed'
+                'for deep learning projects and experiments that require processing patch data.'
+)
