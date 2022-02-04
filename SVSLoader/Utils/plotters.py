45502c18232e36fe825769302fc68b50f1829729
Benjamin Wilson <benjamintaya0111@gmail.com>
2022-02-04 14:51:30 +0000

    -Minor tidying of plotters.py

--- SVSLoader/Utils/plotters.py
+++ SVSLoader/Utils/plotters.py
@@ -3,6 +3,16 @@ import pandas as pd
 import seaborn as sb
 import numpy as np
 
+COMPONENTS = ('0: Non-Inf',
+              '1: Tumour',
+              '2: Str/Fib',
+              '3: Necr',
+              '4: Vessel',
+              '5: Infl',
+              '6: TumLu',
+              '7: Mucin',
+              '8: Muscle')
+
 
 def plot_error(file=None):
     with open(file=file) as error:
@@ -13,31 +23,13 @@ def plot_error(file=None):
         plt.show()
 
 
-def plot_confusion(y_true=None, y_pred=None, title='Confusion Matrix', fmt='d', labels=None):
+def plot_confusion(y_true=None, y_pred=None, title='Confusion Matrix', fmt='d', labels=COMPONENTS):
     cm = {'Actual': y_true, 'Predicted': y_pred}
     cm = pd.DataFrame(cm, columns=cm.keys())
     confusion_matrix = pd.crosstab(cm['Actual'], cm['Predicted'], rownames=['Actual'], colnames=['Predicted'])
     sb.heatmap(confusion_matrix, cmap='Oranges', annot=True, fmt=fmt, robust=True, yticklabels=labels, cbar=False,
                annot_kws={'size': 14})
     plt.title(title)
-    plt.show()
-
-
-def bland_altman_plot(data1, data2, *args, **kwargs):
-    data1 = np.asarray(data1)
-    data2 = np.asarray(data2)
-    mean = np.mean([data1, data2], axis=0)
-    diff = data1 - data2  # Difference between data1 and data2
-    md = np.mean(diff)  # Mean of the difference
-    sd = np.std(diff, axis=0)  # Standard deviation of the difference
-    plt.scatter(mean, diff, *args, **kwargs)
-    plt.axhline(md, color='red', linestyle='--', label='mean')
-    plt.axhline(md + 1.96 * sd, color='gray', linestyle='--', label='SD +1')
-    plt.axhline(md - 1.96 * sd, color='gray', linestyle='--', label='SD -1')
-    plt.xlabel('Average Between Observer and Model')
-    plt.ylabel('Difference between TCD Truth and TCD Predictions')
-    plt.legend()
-    plt.title('Bland-Altman Plot of TCD agreement')
     plt.show()
 
 
