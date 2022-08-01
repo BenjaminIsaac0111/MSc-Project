8718d066668da567cb52e17b6511bce2adc5338a
Benjamin Wilson <scbiw@leeds.ac.uk>
2022-08-01 17:07:31 +0100

    -Some sample code on bilat and gauss filters to possibly experiment with.

--- SVSLoader/InferenceUtils/tf_customs.py
+++ SVSLoader/InferenceUtils/tf_customs.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow.keras.losses
+from tensorflow.keras import backend
 
 
 class Linear(tensorflow.keras.layers.Layer):
@@ -28,3 +29,35 @@ class Linear(tensorflow.keras.layers.Layer):
     def call(self, inputs):
         return tensorflow.keras.layers.LeakyReLU(alpha=0.01)(
             inputs + tf.broadcast_to(self.b, [self.op_shape[0], self.op_shape[1], self.units]))
+
+
+# TODO maybe a possible route but will leave for now.
+class DCRF(tensorflow.keras.layers.Layer):
+    def __init__(self, output_dim, **kwargs):
+        self.output_dim = output_dim
+        self.bilateral = None
+        self.gaussian = None
+
+        super(DCRF, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.bilateral = self.add_weight(
+            name='bilateral',
+            shape=(input_shape[1], self.output_dim),
+            initializer='Ones',
+            trainable=True
+        )
+        self.gaussian = self.add_weight(
+            name='gaussian',
+            shape=(input_shape[1], self.output_dim),
+            initializer='Ones',
+            trainable=True
+        )
+
+        super(DCRF, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, input_data):
+        return backend.dot(input_data, self.kernel)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], self.output_dim
